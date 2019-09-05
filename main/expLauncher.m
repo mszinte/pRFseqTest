1e5c31f43b2633721fc2c1b60812fd4ccdba69cb
mszinte <martin.szinte@gmail.com>
2019-09-05 09:13:40 +0200

    attempt codes daq

--- main/expLauncher.m
+++ main/expLauncher.m
@@ -56,8 +56,8 @@ const.cond_run_num =  [1;1;...
 
 % Desired screen setting
 % ----------------------
-const.desiredFD         =   120;            % Desired refresh rate
-%fprintf(1,'\n\n\tDon''t forget to change before testing\n');
+const.desiredFD         =   60;            % Desired refresh rate
+fprintf(1,'\n\n\tDon''t forget to change before testing\n');
 const.desiredRes        =   [1920,1080];    % Desired resolution
 
 % Path
